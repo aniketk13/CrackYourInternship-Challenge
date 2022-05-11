af7f892194ca5dbb39a300d0759b8daf77ecc2cc
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-05-11 17:33:00 +0530

    Update Best_Time_to_Buy_and_Sell_Stock_II.cpp

--- Best_Time_to_Buy_and_Sell_Stock_II.cpp
+++ Best_Time_to_Buy_and_Sell_Stock_II.cpp
@@ -24,4 +24,39 @@ public:
         }
         return max_profit;
     }
+    
+    //Approach 2
+    int maxProfit(vector<int>& prices) {
+        stack<int> st;
+        int max_profit=0;
+        for(int i=0;i<prices.size();i++){
+            if(st.empty()){
+                st.push(prices[i]);
+            }
+            else{
+                if(prices[i]>st.top())
+                {
+                    max_profit+=prices[i]-st.top();
+                    st.pop();
+                    st.push(prices[i]);
+                }
+                else
+                {
+                    st.pop();
+                    st.push(prices[i]);
+                }
+            }
+        }
+        return max_profit;
+    }
+    
+    //Approach 3
+    int maxProfit(vector<int>& prices) {
+        int max_profit=0;
+        for(int i=0;i<prices.size()-1;i++){
+            if(prices[i+1]-prices[i]>0)
+                max_profit+=prices[i+1]-prices[i];
+        }
+        return max_profit;
+    }
 };
