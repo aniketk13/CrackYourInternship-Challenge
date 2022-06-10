da7e8512a25a29ad3fdbf901048916f42abe79c2
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-06-10 22:28:04 +0530

    Create Right_View_Of_A_Binary_Tree.cpp
    https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ Right_View_Of_A_Binary_Tree.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(!root)
+            return ans;
+        map<int,int>mp;
+        queue<pair<TreeNode*,int>> q;
+        q.push({root,0});
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            TreeNode* node=it.first;
+            int horizontal=it.second;
+            mp[horizontal]=node->val;
+            if(node->left)
+                q.push({node->left,horizontal+1});
+            if(node->right)
+                q.push({node->right,horizontal+1});
+        }
+        for(auto it:mp)
+            ans.push_back(it.second);
+        return ans;
+    }
+};
