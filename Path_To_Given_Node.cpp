4186b1a9f64d159d88b527883da6fc95e765e424
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-06-12 15:22:07 +0530

    Create Path_To_Given_Node.cpp
    https://www.interviewbit.com/problems/path-to-given-node/

---
+++ Path_To_Given_Node.cpp
@@ -0,0 +1,18 @@
+bool getPath(TreeNode* root,vector<int>& ans,int x){
+    if(!root)
+        return false;
+    ans.push_back(root->val);
+    if(root->val==x)
+        return true;
+    if(getPath(root->left,ans,x) || getPath(root->right,ans,x))
+        return true;
+    ans.pop_back();
+    return false;
+}
+vector<int> Solution::solve(TreeNode* A, int B) {
+    vector<int> ans;
+    if(!A)
+        return ans;
+    getPath(A,ans,B);
+    return ans;
+}
