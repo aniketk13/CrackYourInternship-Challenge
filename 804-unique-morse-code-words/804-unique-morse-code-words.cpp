5f293af2118fcd048eebb5ccd8f8bd39c4a269d0
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-08-17 13:29:21 +0530

    Time: 4 ms (71.25%), Space: 8.6 MB (30.73%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,39 +1,14 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        unordered_map<char,string> mp;
-        mp['a']=".-";
-        mp['b']="-...";        
-        mp['c']="-.-.";        
-        mp['d']="-..";        
-        mp['e']=".";        
-        mp['f']="..-.";        
-        mp['g']="--.";        
-        mp['h']="....";        
-        mp['i']="..";        
-        mp['j']=".---";        
-        mp['k']="-.-";        
-        mp['l']=".-..";        
-        mp['m']="--";
-        mp['n']="-.";
-        mp['o']="---";        
-        mp['p']=".--.";        
-        mp['q']="--.-";        
-        mp['r']=".-.";        
-        mp['s']="...";        
-        mp['t']="-";        
-        mp['u']="..-";        
-        mp['v']="...-";        
-        mp['w']=".--";        
-        mp['x']="-..-";        
-        mp['y']="-.--";        
-        mp['z']="--..";
         
-        set<string> ans;
-        for(int i=0;i<words.size();i++){
+        vector<string> codes={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        unordered_set<string> ans;
+        for(string i:words){
             string code="";
-            for(int j=0;j<words[i].length();j++){
-                code+=mp[words[i][j]];
+            for(char j:i){
+                code+=codes[j-'a'];
             }
             ans.insert(code);
             cout<<code;
